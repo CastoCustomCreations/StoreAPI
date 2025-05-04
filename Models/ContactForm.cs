ec70c7b8f7aa0039417998278a5c2dbe36dad793
Jennifer Casto <CastoCustomCreations@gmail.com>
2025-05-04 18:10:56 -0400

    fixed error by making Name nullable

--- Models/ContactForm.cs
+++ Models/ContactForm.cs
@@ -7,7 +7,7 @@ public partial class ContactForm
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
     public string? Email { get; set; } 
 
